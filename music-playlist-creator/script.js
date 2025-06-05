59b4217e9a02d284086bbf82fa51d1afc4e8ea7e
Osasenaga Oluwagbemiga Obaseki <seki@meta.com>
2025-06-05 10:07:30 -0700

    added modal overlay and logic to view the modal content

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,14 @@
+const cards = document.querySelectorAll('.cards')
+const modal = document.querySelector('.modal-overlay')
 
+cards.forEach(card =>{
+    card.addEventListener('click', () =>{
+        modal.classList.add('show')
+    })
+})
+
+modal.addEventListener('click', (e) => {
+    if ( e.target === modal){
+        modal.classList.remove('show')
+    }
+})
